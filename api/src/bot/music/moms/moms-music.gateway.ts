aa432feb6596c60a309c99ea1779217c9f0e2520
Jean-François Sergerie <46312477+jfs39@users.noreply.github.com>
2024-02-20 00:31:01 -0500

    feat: add jf sounds on join

--- api/src/bot/music/moms/moms-music.gateway.ts
+++ api/src/bot/music/moms/moms-music.gateway.ts
@@ -2,14 +2,14 @@ import { probability } from '$common/utils/funcs';
 import { On } from '@discord-nestjs/core';
 import { Controller, Logger } from '@nestjs/common';
 import { VoiceState } from 'discord.js';
-import { FRANCOIS_USER_ID, NICO_USER_ID } from '../constants/moms.ids';
+import { FRANCOIS_USER_ID, NICO_USER_ID, JF_USER_ID } from '../constants/moms.ids';
 import { JoinType, MomsMusicService } from './moms-music.service';
 
 @Controller()
 export class MomsMusicGateway {
 	private readonly logger = new Logger(MomsMusicGateway.name);
 
-	constructor(private readonly momsMusicService: MomsMusicService) {}
+	constructor(private readonly momsMusicService: MomsMusicService) { }
 
 	@On('voiceStateUpdate')
 	async onFrancoisJoin(oldVoiceState: VoiceState, newVoiceState: VoiceState) {
@@ -77,5 +77,63 @@ export class MomsMusicGateway {
 				return probability(percentage);
 			},
 		});
+	}
+
+	@On('voiceStateUpdate')
+	async onJfJoin(oldVoiceState: VoiceState, newVoiceState: VoiceState) {
+		const state = this.momsMusicService.getMemberState({
+			userId: JF_USER_ID,
+			oldVoiceState,
+			newVoiceState,
+			joinType: JoinType.ONLY_USER,
+		});
+
+		if (!state) {
+			return;
+		}
+
+		const { member: jfMember, voiceState } = state;
+
+		const numberOfMinutes = 50;
+
+		const allQuerys = [
+			'https://youtu.be/NPbWhDaESds?si=sa1L5YiJd-yiLww7',
+			'https://youtu.be/j5BXUF_4PP0?si=ubkyzLoXaSVGmAWy',
+			'https://youtu.be/opBFaCS_PV4?si=jtn_UY2v39G7D1wO',
+			'https://youtu.be/kpwNjdEPz7E?si=lFJfVm2V_BcbpAmO',
+			'https://youtu.be/PaOP88t298E?si=bLpFzI6v74e735ls',
+			'https://youtu.be/f8mL0_4GeV0?si=SnuvLD8wbUtQqepp',
+			'https://youtu.be/x7qiftEnQOc?si=AY1bnSKNQW8KdUAK',
+			'https://youtu.be/iBAEt06J2Ho?si=sVjT0f0Dm2YjZM-j',
+			'https://youtu.be/b3FJgIZVW4g?si=Xmxl8wCMebLGFoPQ',
+			'https://youtu.be/8CcP8hbZpew?si=EE71Dg-gDHPUQGLn',
+			'https://youtu.be/IkhrZcgHAwU?si=lNyANOGd4J8S2awm',
+			'https://youtu.be/mdUlSd2Md0M?si=hRd3Ev9nEubOhLVc',
+			'https://youtu.be/3yVd52L748U?si=xQaLsIYed4E2SCUP',
+			'https://youtu.be/GbaHFNu_bAM?si=FW9Xj86B7DoicdYY',
+			'https://youtu.be/JqwMdzrqZn8?si=BwnJ2LQ2lXVuwDKV',
+			'https://youtu.be/TSkJw8KvwbM?si=i3lxwD9Ym9G7N_kX'
+		];
+
+		const query = allQuerys[this.randomIntFromInterval(0, (allQuerys.length - 1))];
+
+		await this.momsMusicService.playThemeIfAwayFor({
+			voiceChannel: voiceState.channel,
+			user: jfMember.user,
+			query,
+			timeout: numberOfMinutes,
+			doCreateLog: (lastLogInTimeout) => {
+				return !lastLogInTimeout || jfMember == voiceState.member;
+			},
+			doPlayMusic: () => {
+				const percentage = 35;
+
+				return probability(percentage);
+			},
+		});
+	}
+
+	private randomIntFromInterval(min: number, max: number) {
+		return Math.floor(Math.random() * (max - min + 1) + min)
 	}
 }
